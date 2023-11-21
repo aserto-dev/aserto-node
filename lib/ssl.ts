776e26171f1a98f8b74163423b3d680007bd2922
Gimmy <gheorghe.popescu@aserto.com>
2023-11-21 10:39:19 +0200

    create default ssl when no ca file path

--- lib/ssl.ts
+++ lib/ssl.ts
@@ -1,9 +1,15 @@
 import { readFileSync } from "fs";
 import { ChannelCredentials, credentials } from "@grpc/grpc-js";
 
-const getSSLCredentials: (ca: string) => ChannelCredentials = (ca) => {
-  const root_cert = readFileSync(ca);
-  return credentials.createSsl(root_cert);
+const getSSLCredentials: (ca: string | undefined) => ChannelCredentials = (
+  ca
+) => {
+  if (!!ca) {
+    const root_cert = readFileSync(ca);
+    return credentials.createSsl(root_cert);
+  }
+
+  return credentials.createSsl();
 };
 
 export default getSSLCredentials;
