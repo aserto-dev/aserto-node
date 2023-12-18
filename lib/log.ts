b679ea87b5af8a18818fd990f33a1b79cf1d7a78
Gimmy <gheorghe.popescu@aserto.com>
2023-12-18 13:58:06 +0200

    add tracing flags

--- lib/log.ts
+++ lib/log.ts
@@ -8,15 +8,20 @@ const logLevels = {
 
 const log = (message: string, level = "INFO") => {
   const timestamp = new Date().toISOString();
-  if (level === "ERROR") {
+  if (process.env.NODE_TRACE) {
     // eslint-disable-next-line no-console
-    console.error(`${timestamp} ${level}: ${message}`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    /* @ts-ignore */
-    //TODO: Remove this ts-ignore
-  } else if (logLevels[level] <= logLevels[currentLevel]) {
-    // eslint-disable-next-line no-console
-    console.log(`${timestamp} ${level}: aserto-node: ${message}`);
+    console.trace(`${timestamp} ${level}: ${message}`);
+  } else {
+    if (level === "ERROR") {
+      // eslint-disable-next-line no-console
+      console.error(`${timestamp} ${level}: ${message}`);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      /* @ts-ignore */
+      //TODO: Remove this ts-ignore
+    } else if (logLevels[level] <= logLevels[currentLevel]) {
+      // eslint-disable-next-line no-console
+      console.log(`${timestamp} ${level}: aserto-node: ${message}`);
+    }
   }
 };
 
