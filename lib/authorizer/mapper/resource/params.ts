b66f75dabf246008d2afbf9eaedd30aca13b7b6b
Gimmy <gheorghe.popescu@aserto.com>
2024-03-21 10:33:31 +0200

    update ResourceParamsMapper

--- lib/authorizer/mapper/resource/params.ts
+++ lib/authorizer/mapper/resource/params.ts
@@ -3,7 +3,7 @@ import { Request } from "express";
 import { ResourceContext } from "../../model/resourceContext";
 
 const ResourceParamsMapper = (req: Request): ResourceContext => {
-  return req.params || {};
+  return req.params;
 };
 
 export default ResourceParamsMapper;
