a543d335a2026d088aff785187513c129feff183
Gimmy <gheorghe.popescu@aserto.com>
2025-03-03 09:17:33 +0200

    update dependecies and remove deprecated packages

--- lib/directory/v3/serializer.ts
+++ lib/directory/v3/serializer.ts
@@ -1,9 +1,3 @@
-import { file_aserto_directory_common_v3_common } from "@aserto/node-directory/src/gen/cjs/aserto/directory/common/v3/common_pb";
-import { file_aserto_directory_exporter_v3_exporter } from "@aserto/node-directory/src/gen/cjs/aserto/directory/exporter/v3/exporter_pb";
-import { file_aserto_directory_importer_v3_importer } from "@aserto/node-directory/src/gen/cjs/aserto/directory/importer/v3/importer_pb";
-import { file_aserto_directory_model_v3_model } from "@aserto/node-directory/src/gen/cjs/aserto/directory/model/v3/model_pb";
-import { file_aserto_directory_reader_v3_reader } from "@aserto/node-directory/src/gen/cjs/aserto/directory/reader/v3/reader_pb";
-import { file_aserto_directory_writer_v3_writer } from "@aserto/node-directory/src/gen/cjs/aserto/directory/writer/v3/writer_pb";
 import type {
   DescEnum,
   DescExtension,
@@ -12,6 +6,13 @@ import type {
   DescService,
   Registry,
 } from "@bufbuild/protobuf";
+
+import { file_aserto_directory_common_v3_common } from "@aserto/node-directory/src/gen/cjs/aserto/directory/common/v3/common_pb";
+import { file_aserto_directory_exporter_v3_exporter } from "@aserto/node-directory/src/gen/cjs/aserto/directory/exporter/v3/exporter_pb";
+import { file_aserto_directory_importer_v3_importer } from "@aserto/node-directory/src/gen/cjs/aserto/directory/importer/v3/importer_pb";
+import { file_aserto_directory_model_v3_model } from "@aserto/node-directory/src/gen/cjs/aserto/directory/model/v3/model_pb";
+import { file_aserto_directory_reader_v3_reader } from "@aserto/node-directory/src/gen/cjs/aserto/directory/reader/v3/reader_pb";
+import { file_aserto_directory_writer_v3_writer } from "@aserto/node-directory/src/gen/cjs/aserto/directory/writer/v3/writer_pb";
 import {
   createRegistry,
   Message,
@@ -28,12 +29,12 @@ class DsRegistry {
 
   constructor(
     ...input: (
-      | Registry
-      | DescFile
-      | DescMessage
       | DescEnum
       | DescExtension
+      | DescFile
+      | DescMessage
       | DescService
+      | Registry
     )[]
   ) {
     this.registry = createRegistry(
